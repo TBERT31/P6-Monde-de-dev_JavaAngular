126db9f34cc3cf66c17d69c744000c27543f6ed2
TBERT31 <berteau.thomas@hotmail.fr>
2024-06-14 02:58:58 +0200

    layout or not ?

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
+
+  
   constructor(
   ) {}
 
